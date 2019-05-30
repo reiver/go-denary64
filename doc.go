acb74090582e8255f88f300905f0b160c7a801b9
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-05-30 10:21:18 -0700

    doc

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+Package denary64 provides a base-10 floating point number type.
+
+Base-10 floating point numbers are safe to use to store money values, and are safe to do math calculations with on money values.
+
+This is as opposed to the built-in Golang types float32, and float64 which are base-2 floating point number types (rather than base-10) and which are NOT safe to use for money values.
+*/
+package denary64
