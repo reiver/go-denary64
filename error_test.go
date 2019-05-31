68127b20527cdab99bb2eaeff13ca6927ee1f7e5
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-05-30 21:11:01 -0700

    denary64.Err()

---
+++ error_test.go
@@ -0,0 +1,59 @@
+package denary64
+
+import (
+	"fmt"
+
+	"testing"
+)
+
+func TestErrNil(t *testing.T) {
+
+	var result Result = Err(nil)
+
+	if None() != result {
+		t.Errorf("Expected None(), but actually got %#v", result)
+		return
+	}
+}
+
+func TestErr(t *testing.T) {
+
+	tests := []struct{
+		Err      error
+		Expected error
+	}{}
+
+	for i:=0; i<100; i++ {
+		err := fmt.Errorf("Error #%d", i)
+
+		test := struct{
+			Err      error
+			Expected error
+		}{
+			Err:      err,
+			Expected: err,
+		}
+
+		tests = append(tests, test)
+	}
+
+	for testNumber, test := range tests {
+
+		var actual Result = Err(test.Err)
+
+		if None() == actual {
+			t.Errorf("For test #%d, did not expect denary.None(), but actually got %#v", testNumber, actual)
+			continue
+		}
+
+		if nil == actual.err {
+			t.Errorf("For test #%d, did not expect internal err to be nil, but actually is %#v", testNumber, actual.err)
+			continue
+		}
+
+		if expected := Err(test.Err); expected != actual {
+			t.Errorf("For test #%d, expected #%v, but actually got %#v", testNumber, expected, actual)
+			continue
+		}
+	}
+}
