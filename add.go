baaedce1ff57880cb992d0d02d5e71cc99c01326
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-06-03 01:09:18 -0700

    created func add() that does a uint64 addition (i.e., +) but also detects overflow, and returns an error when overflow happens.

--- add.go
+++ add.go
@@ -6,7 +6,6 @@ func add(left uint64, right uint64) (uint64, error) {
 	result := left + right
 
 	if (result < left && right > 0) || (result < right && left > 0) {
-//@TODO: return a typed error! so that it can be detected with a Golang type switch.
 		return result, overflowf("%d + %d", left, right)
 	}
 
